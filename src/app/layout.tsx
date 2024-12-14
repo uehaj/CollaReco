c578990d5ddef4a293f86680e185ad6fcde5c51e
Junji UEHARA <uehara.junji@ntt-tx.co.jp>
2024-12-14 16:06:32 +0900

    finx ColaReco->CollaReco

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-  title: "ColaReco",
+  title: "CollaReco",
   description: "Colaborative Speech Recognition",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
